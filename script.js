5879ce8de2dfee5ce69d4b31eb855d41e213c5f4
Kaung722 <70074279+Kaung722@users.noreply.github.com>
2021-07-05 14:34:39 -0700

    added submit listener

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const addTask = function (event) {
+  event.preventDefault();
+  if (document.querySelector("#addTask").value !== "")
+    console.log(document.querySelector("#addTask").value);
+  document.forms[0].reset();
+};
+
+document.querySelector("#btn").addEventListener("click", addTask);
