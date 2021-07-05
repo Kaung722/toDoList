97db2c111ad999c52e2a3eb9054ad5108646de3f
Kaung722 <70074279+Kaung722@users.noreply.github.com>
2021-07-05 15:38:28 -0700

    added unordered list

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 "use strict";
 
+const tasks = [];
+let ul = document.getElementById("taskList");
+
 const addTask = function (event) {
   event.preventDefault();
-  if (document.querySelector("#addTask").value !== "")
-    console.log(document.querySelector("#addTask").value);
+  if (document.querySelector("#addTask").value !== "") {
+    tasks.push(document.querySelector("#addTask").value);
+    let li = document.createElement("li");
+    let textNode = document.createTextNode(
+      document.querySelector("#addTask").value
+    );
+    li.appendChild(textNode);
+    ul.appendChild(li);
+  }
   document.forms[0].reset();
+  console.log(tasks);
 };
 
 document.querySelector("#btn").addEventListener("click", addTask);
