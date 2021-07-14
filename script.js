58c288a2ddcbdac8347ccf208aa2bef937080654
Nay Toe <ksithu722@gmail.com>
2021-07-14 15:47:18 -0700

    added remove

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 "use strict";
 
-//variable
-const tasks = [];
 //selector
 let toDo = document.querySelector("#addTask");
 let toDoList = document.getElementById("taskList");
 let toDoBtn = document.querySelector("#btn");
 //listener
 toDoBtn.addEventListener("click", addTask);
+toDoList.addEventListener("click", removeTask);
 //function
 
 function addTask(event) {
   event.preventDefault();
   if (toDo.value !== "") {
-    tasks.push(toDo.value);
     toDoList.appendChild(createNewList());
   }
   document.forms[0].reset();
-  console.log(tasks); ///////////////////////////testing
+}
+
+function removeTask(event) {
+  toDoList.removeChild(event.target.parentElement.parentElement);
 }
 
 function createNewList() {
